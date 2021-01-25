4514f87a678d7bcebb1664796d4d62b88512dcc9
dean <dean.sindorf@gmail.com>
2021-01-25 14:22:09 -0700

    added untracked files

---
+++ Shaders/geometry_funcs.h
@@ -0,0 +1,77 @@
+// ########## begin geometry_funcs.frag #########################
+
+#define PRODUCE_VERTICES\
+	float dt = 1.0 / float( TESSLVL ); \
+	float t_top = 1.0; \
+	for( int it = 0; it < TESSLVL; it++ ){ \
+		float t_bot = t_top - dt;\
+		float smax_top = 1.0 - t_top;\
+		float smax_bot = 1.0 - t_bot;\
+		int nums = it + 1;\
+		float ds_top = smax_top / float(nums - 1 );\
+		float ds_bot = smax_bot / float(nums);\
+		float s_top = 0.0;\
+		float s_bot = 0.0;\
+		int j=0;\
+		for( int is = 0; is < nums; is++ ,j++)\
+		{\
+			ProduceVertex(s_bot, t_bot);\
+			ProduceVertex(s_top, t_top);\
+			s_top += ds_top;\
+			s_bot += ds_bot;\
+		}\
+		ProduceVertex(s_bot, t_bot);\
+		EndPrimitive();\
+		t_top = t_bot;\
+		t_bot -= dt;\
+	}
+#ifdef _NORMALS_
+#define SET_ZNOISE(func) \
+	v1= Vertex1.xyz; \
+ 	gv = func; \
+ 	g= gv.x; \
+ 	if(lighting) { \
+ 	    float delta=1e-5; \
+ 	    float nbamp = 5e-4/delta; \
+		v1 = vec3(Vertex1.x+delta,Vertex1.y,Vertex1.z);  \
+ 		gv = func; \
+		df.x =gv.x; \
+		v1 = vec3(Vertex1.x,Vertex1.y+delta,Vertex1.z); \
+ 		gv = func; \
+		df.y =gv.x; \
+		v1 = vec3(Vertex1.x,Vertex1.y,Vertex1.z+delta); \
+ 		gv = func; \
+		df.z =gv.x; \
+		df = (df-vec3(g,g,g))*(nbamp); \
+    } \
+  	vec3 v=p.xyz+pv; \
+	v=normalize(v)*g; \
+	p.xyz+=v;	\
+	gl_Position=gl_ModelViewProjectionMatrix * p; \
+	vec3 normal=normalize(Normal.xyz-2e5*gl_NormalMatrix *df); \
+	Normal.xyz=normalize(normal.xyz);
+#else
+#define SET_ZNOISE(func) \
+	v1= Vertex1.xyz; \
+ 	gv = func; \
+ 	g=gv.x; \
+ 	vec3 v=p.xyz+pv; \
+	v=normalize(v)*g; \
+	p.xyz+=v;	\
+	gl_Position=gl_ModelViewProjectionMatrix * p
+#endif
+
+#define NOISE_COLOR(func) \
+	v1= Vertex1.xyz; \
+    vec4 ncolor=func; \
+ 	color =ncolor+color;
+
+#define NOISE_VARS \
+    df=vec3(0); \
+    nbamp=0.0; \
+    g=0.0; \
+    b=0.0; \
+    noise_fade=0.0; \
+	gv=vec4(0.0);
+
+// ########## end geometry_funcs.frag #########################
